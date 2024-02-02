eb2189a91f6cbbfb112b53bf4700943dc57b3197
andre fredriksen <andrefre@kth.se>
2024-02-02 11:25:39 +0100

    #44 Feat: Added a main function to MissileSystem.java

--- 1-DECIDE/src/MissileSystem.java
+++ 1-DECIDE/src/MissileSystem.java
@@ -52,4 +52,47 @@ public class MissileSystem{
     }
     
 
+    public static void main(String args[]){
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 2);
+        Point p3 = new Point(1, 3);
+        Point p4 = new Point(1, 4);
+        Point p5 = new Point(1, 5);
+        Point p6 = new Point(2, 1);
+        Point p7 = new Point(3, 1);
+        Point p8 = new Point(4, 1);
+        Point p9 = new Point(5, 1);
+        Point p10 = new Point(6, 1);
+        Point[] points = {p1, p2, p3, p4, p5, p6, p7, p8, p9, p10};
+
+        int[][] lcm = {
+            {0, 1, 0, 1, 1, 0, 1, 2, 2, 1, 2, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 2},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0}
+        };
+
+        boolean[] puv = {true, false, false, false, true, 
+            false, false, false, false, false,
+            false, false, true, true, true};
+
+        Parameters parameters = new Parameters();
+
+        MissileSystem system = new MissileSystem(points.length, points, lcm, puv, parameters);
+
+        system.decide();
+    }
+
 }
